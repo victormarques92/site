f71b3c06027766779e6ab5759722cfbc4ab18528
Victor Marques <victormatheus92@gmail.com>
2020-10-13 12:56:01 -0300

    Updates

---
+++ precache-manifest.303155f459b3b26bdc564aa80c7aa91c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7430526b060873e9f259c658d482f72a",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "3647c6d55b1dfbcf2cdd",
+    "url": "/site/static/js/2.935e819e.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/site/static/js/2.935e819e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cbe0eae98a68be69fc",
+    "url": "/site/static/js/main.120d3224.chunk.js"
+  },
+  {
+    "revision": "c5bd34fd4839781667f9",
+    "url": "/site/static/js/runtime-main.85b6f7e0.js"
+  }
+]);
